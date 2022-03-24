bde994840cd0cf6f20f442149fecf94b608f3ee8
fikrimus35 <mustofa13.fikri@gmail.com>
2022-03-24 11:02:49 +0700

    Get user and show to layout, add logout functionality

--- app/src/main/java/com/kelompok13/tugas1/MainActivity.java
+++ app/src/main/java/com/kelompok13/tugas1/MainActivity.java
@@ -2,13 +2,32 @@ package com.kelompok13.tugas1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String USER_EXTRA = "user_extra";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String user = intent.getStringExtra(USER_EXTRA);
+
+        TextView greetingTv = findViewById(R.id.greetingTV);
+        greetingTv.setText(getResources().getString(R.string.greeting_message, user));
+
+        Button logoutBtn = findViewById(R.id.logoutBtn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
